cdb0f046f5883459d0a59c1d80ce7fd13c0f1944
alec.chepkin <alec.chepkin@gmail.com>
2024-12-08 11:05:51 +0000

    feat: add support for graceful shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gorilla/mux"
 
@@ -17,9 +22,7 @@ func main() {
 
 	r := mux.NewRouter()
 
-	//r.HandleFunc("/accounts", handlers.CreateAccount).Methods("POST")
 	r.HandleFunc("/accounts/{id}", p.AccountHandler().GetAccount).Methods("GET")
-	// Create Account
 	r.HandleFunc("/accounts", p.AccountHandler().CreateAccount).Methods("POST")
 	r.HandleFunc("/accounts", p.AccountHandler().ListAccounts).Methods("GET")
 	r.HandleFunc("/accounts/{id}/transactions", p.TransactionHandler().CreateTransaction).Methods("POST")
@@ -28,33 +31,32 @@ func main() {
 
 	log.Printf("Starting HTTP server on http://localhost%s", cfg.HTTPAddr)
 
-	//srv := &http.Server{
-	//	Handler: r,
-	//	Addr:    cfg.HTTPAddr,
-	//}
+	srv := &http.Server{
+		Handler: r,
+		Addr:    cfg.HTTPAddr,
+	}
 
-	//// listen to OS signals and gracefully shutdown HTTP server
-	//stopped := make(chan struct{})
-	//go func() {
-	//	sigint := make(chan os.Signal, 1)
-	//	signal.Notify(sigint, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
-	//	<-sigint
-	//	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
-	//	_ = ctx
-	//	defer cancel()
-	//	if err := srv.Shutdown(ctx); err != nil {
-	//		log.Printf("HTTP Server Shutdown Error: %v", err)
-	//	}
-	//	close(stopped)
-	//}()
+	// listen to OS signals and gracefully shutdown HTTP server
+	stopped := make(chan struct{})
+	go func() {
+		sigint := make(chan os.Signal, 1)
+		signal.Notify(sigint, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+		<-sigint
+		ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+		_ = ctx
+		defer cancel()
+		if err := srv.Shutdown(ctx); err != nil {
+			log.Printf("HTTP Server Shutdown Error: %v", err)
+		}
+		close(stopped)
+	}()
 
-	//log.Fatal(srv.ListenAndServe())
 	err := http.ListenAndServe(cfg.HTTPAddr, r)
 	if err != nil {
 		log.Fatalf("HTTP server ListenAndServe Error: %v", err)
 	}
 
-	//<-stopped
-	//
-	//log.Printf("Have a nice day!")
+	<-stopped
+
+	log.Printf("Have a nice day!")
 }
